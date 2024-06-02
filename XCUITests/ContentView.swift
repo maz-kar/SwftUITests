ff6b9713c42736dadda50640b4e21e5b43e3d425
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-02 19:13:31 +0200

    Initial Commit

---
+++ XCUITests/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCUITests
+//
+//  Created by Maziar Layeghkar on 02.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCUITests/XCUITestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUITestsApp.swift
+//  XCUITests
+//
+//  Created by Maziar Layeghkar on 02.06.24.
+//
+
+import SwiftUI
+
+@main
+struct XCUITestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
